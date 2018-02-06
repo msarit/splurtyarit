6947e5c79b965c691fcdc6435a1eee0593dbb065
Arit <msarit@gmail.com>
2018-02-06 21:29:07 +0000

    Make Quotes Form Functional

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -6,4 +6,15 @@ class QuotesController < ApplicationController
   def new
     @quote = Quote.new
   end
+
+  def create
+    Quote.create(quote_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def quote_params
+    params.require(:quote).permit(:saying, :author)
+  end
 end
