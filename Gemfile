1355bc5c510bdcf147f17c5f1a22f5afadbadd80
Arit <msarit@gmail.com>
2018-02-06 20:41:56 +0000

    Build Contribute Form and Connect Links for Logo, Footer and Contribute Nav Item

--- Gemfile
+++ Gemfile
@@ -54,3 +54,5 @@ gem 'bootstrap', '4.0.0.alpha6'
 source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
 end
+
+gem 'simple_form'
--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,4 +2,8 @@ class QuotesController < ApplicationController
   def index
     @quote = Quote.order("RANDOM()").first
   end
+
+  def new
+    @quote = Quote.new
+  end
 end
