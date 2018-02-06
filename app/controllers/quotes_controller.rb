e71563098b2646789fd4cab22f7979c3795ebd1f
Arit <msarit@gmail.com>
2018-02-06 23:21:07 +0000

    Lesson 15: Build About Page out

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -15,6 +15,9 @@ class QuotesController < ApplicationController
     redirect_to root_path
   end
 
+  def about
+  end
+
   private
 
   def quote_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'quotes#index'
   resources :quotes
+  get 'about', to: 'quotes#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
